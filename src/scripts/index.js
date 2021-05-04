2ca07f4b011152896893ac3dbc03c96bb49616c9
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-05-03 20:55:55 -0400

    Listo el carrito, diseño y responsive. Todo listo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,10 +9,17 @@ const Nothing = `
         `;
 let carrito = {};
 let TotalPrice = 0;
+const Buy = document.getElementById('buy');
+const ShoppingCartAmount = document.getElementById('shopping-cart__amount');
 // Funciones
 
 document.addEventListener('DOMContentLoaded', () => {
     FetchData();
+    MobileCart();
+});
+
+window.addEventListener('resize', () => {
+    MobileCart();
 });
 
 const FetchData = async () => {
@@ -32,7 +39,7 @@ const ShowProducts = (Data) => {
         CheckOut.innerHTML = Nothing;
         CheckOutOtherInfo.innerHTML = '';
         CheckOut.classList.add('checkout--no-items');
-        PaypalContainer.style.display = 'none';
+        PaypalContainer.className = 'paypal-button-container--none';
     }
 
     Data.forEach((product) => {
@@ -112,17 +119,15 @@ const AddToCart = () => {
     if (CheckOut.hasChildNodes() == false) {
         CheckOut.innerHTML = Nothing;
         CheckOut.classList.add('checkout--no-items');
-        PaypalContainer.style.display = 'none';
+        PaypalContainer.className = 'paypal-button-container--none';
     } else {
         CheckOut.classList.remove('checkout--no-items');
-        PaypalContainer.style.display = 'block';
+        PaypalContainer.className = 'paypal-button-container';
     }
 
     Object.values(carrito).forEach((product) => {
         const price = product.price * product.quantity;
         if (product.quantity == 0) {
-            // const X = document.querySelectorAll('');
-            // console.log(X);
             return;
         }
         const quantity =
@@ -195,6 +200,8 @@ const CalcTotal = () => {
     CheckOutTotal.textContent = `Total: ${Price.toFixed(2)}$`;
     CheckOutQuantity.textContent = `Items: ${Quantity}`;
 
+    ShowaAmountOfItems(Quantity);
+
     TotalPrice = Price.toFixed(2);
 };
 
@@ -204,5 +211,38 @@ const RemoveCart = () => {
     CheckOut.innerHTML = Nothing;
     CheckOutOtherInfo.innerHTML = '';
     CheckOut.classList.add('checkout--no-items');
-    PaypalContainer.style.display = 'none';
+    PaypalContainer.className = 'paypal-button-container--none';
+};
+
+const MobileCart = () => {
+    if (window.innerWidth < 640) {
+        OpenCart();
+    } else {
+        Buy.className = 'buy';
+    }
+};
+
+const OpenCart = () => {
+    Buy.classList.replace('buy', 'buy-mobile');
+    const BuyMobile = document.querySelector('.buy-mobile');
+    BuyMobile.addEventListener('click', (e) => {
+        if (
+            e.target.classList == 'shopping-cart__container' ||
+            e.target.classList == 'shopping-cart__icon'
+        ) {
+            Buy.classList.replace('buy-mobile', 'buy-mobile--open');
+            CloseCart();
+        }
+    });
+};
+
+const CloseCart = () => {
+    const CloseIcon = document.querySelector('.close-icon');
+    CloseIcon.addEventListener('click', () => {
+        Buy.classList.replace('buy-mobile--open', 'buy-mobile');
+    });
+};
+
+const ShowaAmountOfItems = (Quantity) => {
+    ShoppingCartAmount.textContent = Quantity;
 };
